294b2ca5d9f87cae869249ae062eaa3dac92ac17
MuqhtadeerM <muhammedmuzawar9@gmail.com>
2026-01-17 01:08:49 +0530

    feat:added token generation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/User.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   try {
@@ -13,11 +15,15 @@ export const registerUser = async (req, res) => {
       });
     }
 
+    // hash paswword
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     //   create new user in database
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     //   send response
@@ -28,6 +34,45 @@ export const registerUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "registation failed",
+      error: error.message,
+    });
+  }
+};
+
+// user login function
+export const loginUser = async (req, res) => {
+  try {
+    // get login data
+    const { email, password } = req.body;
+
+    // check if user exist
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid Email or password" });
+    }
+
+    // compaire the passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid Password or Email",
+      });
+    }
+
+    // generate the tocken using jwt - this will tels the who is the user without storing the data on server side
+    // it varifies the authenticity and generates the tocken which is automatically expires
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    // success response
+    res.status(200).json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Login Failed",
       error: error.message,
     });
   }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import { loginUser, registerUser } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 // POST API
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
